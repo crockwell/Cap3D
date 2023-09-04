c815f9b9202c2895de293f26af8a98f2a7c6877d
Tiange Luo <tiange.cs@gmail.com>
2023-09-04 00:45:07 -0400

    pointE_inference_code

---
+++ text-to-3D/point-e/README.md
@@ -0,0 +1,15 @@
+# Point·E
+
+# Usage
+
+1. Install with `pip install -e .`. Please refer to the [original repo](https://github.com/openai/point-e), if any errors occur.
+
+2. Download finetuned checkpoint from https://huggingface.co/datasets/tiange/Cap3D/tree/main/our_finetuned_models, and move it to `model_ckpts`.
+
+3. execute the below command, the results will saved at `./pointE_inference/Cap3D_test1'`.
+```
+python text2ply_pointE.py
+
+# if you need to generate test meshes for the 2k set:
+python text2ply_pointE.py --test_type '2k'
+```
---
+++ text-to-3D/point-e/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="point-e",
+    packages=[
+        "point_e",
+        "point_e.diffusion",
+        "point_e.evals",
+        "point_e.models",
+        "point_e.util",
+    ],
+    install_requires=[
+        "filelock",
+        "Pillow",
+        "torch",
+        "fire",
+        "humanize",
+        "requests",
+        "tqdm",
+        "matplotlib",
+        "scikit-image",
+        "scipy",
+        "pandas",
+        "IPython",
+        "wandb",
+        "open3d",
+        "numpy",
+        "clip @ git+https://github.com/openai/CLIP.git",
+    ],
+    author="OpenAI",
+)
